1cbdbdc8b936826205b0252b650826ee535a4d4c
Jordan <jordan.bamber@gmail.com>
2021-07-09 11:54:54 -0700

    feat: form functionality and input validation

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { DataService } from '../shared/data.service';
 import { todo } from '../shared/todo.model';
 
@@ -10,11 +11,26 @@ import { todo } from '../shared/todo.model';
 export class TodosComponent implements OnInit {
 
   todoArray: todo[] = [];
+  showValidationErrors: boolean = false;
   
   constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
     this.todoArray = this.dataService.getAllTodos();
+  }
+
+  //Why in this file?
+  onFormSubmit(form: NgForm) {
+
+    if(form.invalid) return this.showValidationErrors = true;
+
+    this.dataService.addTodo(new todo(form.value.text));
+
+    this.showValidationErrors = false;
+    form.reset();
+
+    //added this so always returns. Good idea?
+    return;
   }
 
 }
